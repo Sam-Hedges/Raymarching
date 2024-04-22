f9dcdf25422e4c56388def21f3afe2a5d796bc4e
Sam Hedges <sam.hedges.sh@gmail.com>
2024-04-22 13:20:41 +0100

    Added Bust

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up, 1);
+    }
+}
